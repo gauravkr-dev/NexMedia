59d69c4214e41d338b296375206564de65398316
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-01-08 16:35:52 +0530

    add Clerk authentication integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,16 @@ import Connection from './pages/Connection.jsx'
 import Discover from './pages/Discover.jsx'
 import Profile from './pages/Profile.jsx'
 import CreatePost from './pages/CreatePost.jsx'
+import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/Layout.jsx'
 
 const App = () => {
+  const {user} = useUser();
   return (
     <>
       <Routes>
         {/* Define your routes here */}
-        <Route path='/' element={<Login />}>
+        <Route path='/' element={!user ? <Login /> : <Layout />}>
           <Route index element={<Feed />} />
           <Route path='messages' element={<Messages />} />
           <Route path='messages/:userId' element={<ChatBox />} />
